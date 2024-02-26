762ce6dbf16487cf614e683d79d73866df52bc88
Verónica Domínguez <veronicanicoll97@gmail.com>
2024-02-26 19:38:28 -0300

    Agrega componentes e iconos para el portfolio.
    - Agrega imagenes, agrega CV, entre otros ajustes.
    - TODO: Modificar los comentarios dentro de cada sección.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+const colors = require("tailwindcss/colors");
+const fontSize = require("tailwindcss/defaultTheme").fontSize;
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
   theme: {
     extend: {},
+    colors: {
+      primary: "#0866F1",
+      "primary-dark": "#2377FF",
+      ...colors,
+    },
+    fontSize: {
+      ...fontSize,
+      regular: "15px",
+    },
   },
   plugins: [],
-}
-
+  darkMode: "class",
+};
